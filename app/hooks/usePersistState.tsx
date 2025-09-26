833367c6171f2f3d739296d167bcdabde0be3c62
AmeenCodes01 <masaqib5@gmail.com>
2025-09-26 12:17:07 +0100

    ls er

--- app/hooks/usePersistState.tsx
+++ app/hooks/usePersistState.tsx
@@ -7,20 +7,21 @@ import { useEffect, useMemo, useState } from "react";
  * @param id - A unique key to store and retrieve state in localStorage.
  * @returns [state, setState] tuple like useState
  */
-export default function usePersistState<T>(initialValue: T, id: string): [T, React.Dispatch<React.SetStateAction<T>>] {
-  const _initialValue = useMemo(() => {
-    try {
-      const stored = localStorage.getItem("state:" + id);
-      if (stored) {
-        return JSON.parse(stored);
+export default function usePersistState<T>(
+  initialValue: T,
+  id: string
+): [T, React.Dispatch<React.SetStateAction<T>>] {
+  const [state, setState] = useState<T>(() => {
+    if (typeof window !== "undefined") {
+      try {
+        const stored = localStorage.getItem("state:" + id);
+        if (stored) return JSON.parse(stored);
+      } catch (e) {
+        console.warn("Failed to parse localStorage for:", id, e);
       }
-    } catch (e) {
-      console.warn("Failed to parse localStorage for:", id, e);
     }
     return initialValue;
-  }, [id, initialValue]);
-
-  const [state, setState] = useState<T>(_initialValue);
+  });
 
   useEffect(() => {
     try {
